1e5b45cce5b28094c6d53893b71363da9ddc5167
kpimarianenko <kpimarianenko@gmail.com>
2020-12-16 01:12:28 +0200

    small fixes

---
+++ server/config.js
@@ -0,0 +1,17 @@
+const config = {
+    mongodb: {
+        port: 4000,
+        url: "mongodb+srv://roma123:roma123@cluster0-bsojs.mongodb.net/culture-cloud?retryWrites=true&w=majority" 
+    },
+
+    cloudinary: {
+        api_key: 563491488579862,
+        api_secret: "eTTlyQfGaAZtgzdVmSFFHTr8p0Q",
+        cloud_name: "skybox"
+    },
+
+    secret: 'СulтurеСlоud$ecretKey',
+    salt: 'H1a_c.k,e3r4Se5c.r$e%t@Sa!lt^Fo_-=+rHashPass'
+}
+
+module.exports = config;
